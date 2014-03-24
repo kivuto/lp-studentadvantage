d1e2942da47e2dcf830e25761921e9499bd88df7
iryanclarke <clarke_93@hotmail.com>
2014-03-24 14:55:24 -0400

    CTA rows with email inputs

--- content-email-cta.php
+++ content-email-cta.php
@@ -1,9 +1,9 @@
 
-  <div class="row mainContent content-pricing headline-cta" data-block="<?php echo $block2; ?>">
+  <div class="row mainContent content-email-cta headline-cta" data-block="<?php echo $block2; ?>">
       <div class="container">
           <div class="row">
               <div class="large-12 columns center">
-                    <h1>Low and no cost options available</h1>
+                    <h1>Let's Talk</h1>
               </div> 
           </div> 
           <div class="row">
@@ -13,12 +13,14 @@
           </div>
           <div class="row">
               <div class="large-12 columns info center">
-                  <p>We’re ready for any questions you may have. Let’s talk about how we can help you easily manage the Microsoft Student Advantage program.</p>
+                  <form>
+                      <input type="email" name="email" placeholder="Enter email">
+                  </form>
               </div> 
           </div>
-          <div class="row">
+          <div class="row center">
               <a href="#" target="_blank" onclick="tob(this, '', '', ''); return false;">
-                  <div class="large-12 columns button">
+                  <div class="large-12 columns button center">
                      <span>Submit</span>
                   </div>
               </a>

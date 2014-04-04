31b0817b2d075046a19c2bf51e41bdc2223dc0a8
iryanclarke <clarke_93@hotmail.com>
2014-04-04 10:14:19 -0400

    Mobile tweaking fixes and addition of google analytics demographics

--- tracking.php
+++ tracking.php
@@ -6,6 +6,7 @@
        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
        ga('create', 'UA-2768482-17', 'kivuto.com');
+       ga('require', 'displayfeatures');
        ga('send', 'pageview');
 
     </script>

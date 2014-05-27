c071f2229db416fbad23f39a6dece3effa89aeea
iryanclarke <clarke_93@hotmail.com>
2014-05-27 14:22:32 -0400

    Removal of appostrophe in tracking

--- header.php
+++ header.php
@@ -16,13 +16,13 @@
         <li><a onclick="tib('.content-solution', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Solution');">Solution</a></li>
         <li><a onclick="tib('.content-resources', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Resources');">Resources</a></li>
         <li><a onclick="tib('.content-pricing', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Pricing');">Pricing</a></li>
-        <li><a onclick="tib('.content-email', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Let's talk');">Let's talk</a></li>
+        <li><a onclick="tib('.content-email', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Lets talk');">Let's talk</a></li>
         <li><a onclick="tib('.content-customers', 'Student Advantage LP', 'Student Advantage LP - Navigation', 'Student Advantage LP - Navigation - Header click - Customers');">Our Customers</a></li>
     </ul>
   </aside>
   <a class="exit-off-canvas"></a>
 </div>
-
+ 
 
   <!-- Sub Header/Nav DESKTOPS -->
   <nav class="top-bar subHeader snapNav hide-for-small" data-topbar>

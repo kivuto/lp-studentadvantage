1702bacaddd66d24da0d207965bbd86101ef94e5
iryanclarke <clarke_93@hotmail.com>
2014-05-28 14:49:06 -0400

    Removal of some JS not needed

--- js/main.js
+++ js/main.js
@@ -78,40 +78,6 @@ $(document).ready(function(){
 	}
 	// end async
 
-	/* not working so hot */
-
-	/*$('form').on('submit',function(){
-		event.preventDefault();
-		var email = $('#EmailElement_0_input').val();
-	    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	    var valid = re.test(email);
-	    if(valid==true){
-	    
-	    var formOptions = { 
-			    success: 		function() {
-			    	console.log('success');
-			    	tib(this, 'Student Advantage LP', 'Student Advantage LP - Clickthrough', 'Student Advantage LP - Clickthrough - Let\'s Talk - Email signup');
-				    
-					$('.email input.button').css("opacity", "0");
-	    			$('.row.email').css("opacity", "0");
-	    			$('.row.thank-you').css("opacity", "1.0").css("transform","translateY("+ -155 +"px)");
-
-				    //$('.row.email').css("visibility", "0");
-				    //$('.row.thank-you').css("opacity", "1.0")
-			    }
-			};
-			$('form').ajaxForm(formOptions);
-	    }else{
-	    	$('#EmailElement_0_input').animate({
-	    		opacity : '.5',
-	    	},200).animate({
-	    		opacity : '1',
-	    	},200);
-	    	return false;
-	    }
-	}); */
-
-
 
 	/*--------------------
 

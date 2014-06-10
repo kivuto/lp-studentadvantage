3e8aa6fbdefa695be88dbe05bff9741342b3abd4
iryanclarke <clarke_93@hotmail.com>
2014-06-10 13:52:00 -0400

    Removal of a console.log

--- js/main.js
+++ js/main.js
@@ -48,7 +48,6 @@ $(document).ready(function(){
 				// Track the successful clickthrough
 				tib('', 'Student Advantage LP', 'Student Advantage LP - Clickthrough', 'Student Advantage LP - Clickthrough - Email - Submit');
 				document.getElementById('twitter-pixel').innerHTML = '<script src="//platform.twitter.com/oct.js" type="text/javascript"></script><script type="text/javascript">twttr.conversion.trackPid("l4f2m");</script><noscript><img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4f2m&p_id=Twitter" /><img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=l4f2m&p_id=Twitter" /></noscript>';
-				    console.log('Should have added innerHTML');
 			}else{
 				return true;
 			}
